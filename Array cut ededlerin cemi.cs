40645646baa6b3fef4f14a7cce241cd60d59f0da
anarmh <anarmh@code.edu.az>
2022-10-05 03:09:08 -0700

    Array cut ededlerin cemi

---
+++ Array cut ededlerin cemi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace cut_ededlerin_cemi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PairSum());
+        }
+
+        public static int PairSum()
+        {
+            int sum = 0;
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 ==0) ;
+                 sum += arr[i]; 
+            }
+            return sum;
+        }
+
+    }
+}
