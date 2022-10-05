1363205755d122e37d4ca19683773a870e93df51
anarmh <anarmh@code.edu.az>
2022-10-05 03:21:17 -0700

    cut olanlarin ceminin kvadrati

---
+++ cut olanlarin ceminin kvadrati.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace cut_olanlarin_ceminin_kvadrati
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumSquare());
+        }
+       
+        public static int SumSquare()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int sum = 0;
+            for (int i = 0; i <= arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                    sum += i;
+
+            }
+            return sum*sum;
+
+
+        }
+        
+
+        
+
+
+
+    }
+}
