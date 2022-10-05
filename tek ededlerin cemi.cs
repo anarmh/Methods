00f924233bb1fcd57167b8b434ff523a809cd346
anarmh <anarmh@code.edu.az>
2022-10-05 03:07:24 -0700

    tek ededlerin cemi

---
+++ tek ededlerin cemi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(OddSum(3,20));
+        }
+
+        public static int OddSum(int n,int m)
+        {
+            int sum = 0;
+            if (n < m)
+            {
+                for (int i = n; i < m; i++)
+                {
+                  if  (i % 2 != 0);
+                    sum += i;
+                }
+            }
+            else
+            {
+                for (int i = m; i < n; i++)
+                {
+                    if (i % 2 != 0) ;
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+    }
+}
